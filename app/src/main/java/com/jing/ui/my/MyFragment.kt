20b7cc5976ce35d8e19544412be3ca0496dd8cd7
jing <183685415+lingjing1213@users.noreply.github.com>
2024-10-04 00:16:46 +0800

    权限修改

--- app/src/main/java/com/jing/ui/my/MyFragment.kt
+++ app/src/main/java/com/jing/ui/my/MyFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.jing.databinding.FragmentHomeBinding
 import com.jing.databinding.FragmentMyBinding
 
 class MyFragment : Fragment() {
